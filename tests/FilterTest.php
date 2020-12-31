f20fd36334b48d61c420c6db9e54deca011d2b29
wallace <wallacemaxters@gmail.com>
2020-12-31 01:29:07 -0300

    tab to spaces :heart:

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -4,201 +4,200 @@ use LaravelLegends\EloquentFilter\Filter;
 
 class FilterTest extends Orchestra\Testbench\TestCase
 {
-	protected function getEnvironmentSetUp($app)
-	{
-		// Setup default database to use sqlite :memory:
-		$app['config']->set('database.default', 'testbench');
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
 
-		$app['config']->set('database.connections.testbench', [
-			'driver'   => 'sqlite',
-			'database' => ':memory:',
-			'prefix'   => '',
-		]);
-	}
-
-
-	public function testGetCallback()
-	{
-
-		$filter = new Filter();
-
-		$query = User::query();
-
-		$callback = $filter->getCallback(request());
-
-		$this->assertTrue($callback instanceof \Closure);
-	}
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
 
 
-	public function testApplyMax()
-	{
-		$query = User::query();
+    public function testGetCallback()
+    {
 
-		request()->replace([
-			'max' => ['age' => '18']
-		]);
+        $filter = new Filter();
 
-		(new Filter())->apply($query, request());
+        $query = User::query();
 
-		$this->assertTrue($query->toSql() === 'select * from "users" where ("age" <= ?)');
+        $callback = $filter->getCallback(request());
 
-		$this->assertContains('18', $query->getBindings());
-		
-	}
-
-	public function testApplyMin()
-	{
-		request()->replace([
-			'min' => ['likes' => '500']
-		]);
-
-		$query = User::query();
-
-		(new Filter())->apply($query, request());
-
-		$this->assertTrue($query->toSql() === 'select * from "users" where ("likes" >= ?)');
-
-		$this->assertContains('500', $query->getBindings());
-	}
-
-	public function testApplyExact()
-	{
-		request()->replace([
-			'exact' => ['name' => 'wallace', 'active' => '1']
-		]);
-
-		$query = User::query();
-
-		(new Filter())->apply($query, request());
-
-		$this->assertTrue($query->toSql() === 'select * from "users" where ("name" = ? and "active" = ?)');
-
-		$bindings = $query->getBindings();
-
-		$this->assertContains('wallace', $bindings);
-		$this->assertContains('1', $bindings);
-	}
+        $this->assertTrue($callback instanceof \Closure);
+    }
 
 
-	public function testApplyContains()
-	{
-		request()->replace([
-			'contains' => ['name' => 'wallace']
-		]);
+    public function testApplyMax()
+    {
+        $query = User::query();
 
-		$query = User::query();
+        request()->replace([
+            'max' => ['age' => '18']
+        ]);
 
-		(new Filter())->apply($query, request());
+        (new Filter())->apply($query, request());
 
+        $this->assertTrue($query->toSql() === 'select * from "users" where ("age" <= ?)');
 
-		$this->assertTrue($query->toSql() === 'select * from "users" where ("name" LIKE ?)');
+        $this->assertContains('18', $query->getBindings());
+        
+    }
 
-		$bindings = $query->getBindings();
+    public function testApplyMin()
+    {
+        request()->replace([
+            'min' => ['likes' => '500']
+        ]);
 
-		$this->assertContains('%wallace%', $bindings);
-	}
+        $query = User::query();
 
-	public function testApplyEndsWith()
-	{
-		request()->replace([
-			'ends_with' => ['name' => 'guilherme']
-		]);
+        (new Filter())->apply($query, request());
 
-		$query = User::query();
+        $this->assertTrue($query->toSql() === 'select * from "users" where ("likes" >= ?)');
 
-		(new Filter())->apply($query, request());
+        $this->assertContains('500', $query->getBindings());
+    }
 
-		$this->assertTrue($query->toSql() === 'select * from "users" where ("name" LIKE ?)');
+    public function testApplyExact()
+    {
+        request()->replace([
+            'exact' => ['name' => 'wallace', 'active' => '1']
+        ]);
 
-		$bindings = $query->getBindings();
-		
-		$this->assertContains('%guilherme', $bindings);
-	}
-	
+        $query = User::query();
 
-	public function testApplyStartsWith()
-	{
-		request()->replace([
-			'starts_with' => ['nick' => 'brcontainer']
-		]);
+        (new Filter())->apply($query, request());
 
-		$query = User::query();
+        $this->assertTrue($query->toSql() === 'select * from "users" where ("name" = ? and "active" = ?)');
 
-		(new Filter())->apply($query, request());
+        $bindings = $query->getBindings();
 
-		$this->assertTrue($query->toSql() === 'select * from "users" where ("nick" LIKE ?)');
-
-		$bindings = $query->getBindings();
-		
-		$this->assertContains('brcontainer%', $bindings);
-	}
+        $this->assertContains('wallace', $bindings);
+        $this->assertContains('1', $bindings);
+    }
 
 
-	public function testApplyHas()
-	{
-		request()->replace([
-			'has' => ['phones' => '1', 'documents' => '0']
-		]);
+    public function testApplyContains()
+    {
+        request()->replace([
+            'contains' => ['name' => 'wallace']
+        ]);
 
-		$query = User::query();
+        $query = User::query();
 
-		(new Filter())->apply($query, request());
-
-		$expected_sql = 'select * from "users" where (exists (select * from "user_phones" where "users"."id" = "user_phones"."user_id") and not exists (select * from "user_documents" where "users"."id" = "user_documents"."user_id"))';
-
-		$this->assertTrue($query->toSql() === $expected_sql);
-
-		$this->assertTrue(count($query->getBindings()) === 0);
-	}
+        (new Filter())->apply($query, request());
 
 
-	public function testApplyIsNull()
-	{
-		request()->replace([
-			'is_null' => ['deleted_at' => '1', 'cpf' => '0']
-		]);
+        $this->assertTrue($query->toSql() === 'select * from "users" where ("name" LIKE ?)');
 
-		$query = User::query();
+        $bindings = $query->getBindings();
 
-		(new Filter())->apply($query, request());
+        $this->assertContains('%wallace%', $bindings);
+    }
 
-		$expected_sql = 'select * from "users" where ("deleted_at" is null and "cpf" is not null)';
+    public function testApplyEndsWith()
+    {
+        request()->replace([
+            'ends_with' => ['name' => 'guilherme']
+        ]);
 
-		$this->assertTrue($query->toSql() === $expected_sql);
+        $query = User::query();
 
-		$this->assertTrue(count($query->getBindings()) === 0);
-	}
+        (new Filter())->apply($query, request());
+
+        $this->assertTrue($query->toSql() === 'select * from "users" where ("name" LIKE ?)');
+
+        $bindings = $query->getBindings();
+        
+        $this->assertContains('%guilherme', $bindings);
+    }
+    
+
+    public function testApplyStartsWith()
+    {
+        request()->replace([
+            'starts_with' => ['nick' => 'brcontainer']
+        ]);
+
+        $query = User::query();
+
+        (new Filter())->apply($query, request());
+
+        $this->assertTrue($query->toSql() === 'select * from "users" where ("nick" LIKE ?)');
+
+        $bindings = $query->getBindings();
+        
+        $this->assertContains('brcontainer%', $bindings);
+    }
 
 
-	public function testFromModel()
-	{
-		request()->replace([
-			'max'      => ['age' => '18'],
-			'contains' => ['name' => 'wallace'],
-		]);
+    public function testApplyHas()
+    {
+        request()->replace([
+            'has' => ['phones' => '1', 'documents' => '0']
+        ]);
 
-		$query = Filter::fromModel(User::class, request());
+        $query = User::query();
 
-		$this->assertTrue(
-			$query->toSql() == 'select * from "users" where ("age" <= ? and "name" LIKE ?)'
-		);
+        (new Filter())->apply($query, request());
 
-		$bindings = $query->getBindings();
+        $expected_sql = 'select * from "users" where (exists (select * from "user_phones" where "users"."id" = "user_phones"."user_id") and not exists (select * from "user_documents" where "users"."id" = "user_documents"."user_id"))';
 
-		$this->assertContains('%wallace%', $bindings);
-		$this->assertContains('18', $bindings);
-	}
+        $this->assertTrue($query->toSql() === $expected_sql);
+
+        $this->assertTrue(count($query->getBindings()) === 0);
+    }
+
+
+    public function testApplyIsNull()
+    {
+        request()->replace([
+            'is_null' => ['deleted_at' => '1', 'cpf' => '0']
+        ]);
+
+        $query = User::query();
+
+        (new Filter())->apply($query, request());
+
+        $expected_sql = 'select * from "users" where ("deleted_at" is null and "cpf" is not null)';
+
+        $this->assertTrue($query->toSql() === $expected_sql);
+
+        $this->assertTrue(count($query->getBindings()) === 0);
+    }
+
+    public function testFromModel()
+    {
+        request()->replace([
+            'max'      => ['age' => '18'],
+            'contains' => ['name' => 'wallace'],
+        ]);
+
+        $query = Filter::fromModel(User::class, request());
+
+        $this->assertTrue(
+            $query->toSql() == 'select * from "users" where ("age" <= ? and "name" LIKE ?)'
+        );
+
+        $bindings = $query->getBindings();
+
+        $this->assertContains('%wallace%', $bindings);
+        $this->assertContains('18', $bindings);
+    }
 
 
     public function testApplyIn()
     {
         request()->replace([
-			'in' => ['role_id' => ['1', '2']]
-		]);
+            'in' => ['role_id' => ['1', '2']]
+        ]);
 
-		$query = User::query();
+        $query = User::query();
 
-		(new Filter())->apply($query, request());
+        (new Filter())->apply($query, request());
 
         $expected_sql = 'select * from "users" where ("role_id" in (?, ?))';
 
@@ -206,8 +205,8 @@ class FilterTest extends Orchestra\Testbench\TestCase
         
         $bindings = $query->getBindings();
 
-		$this->assertContains('2', $bindings);
-		$this->assertContains('1', $bindings);
+        $this->assertContains('2', $bindings);
+        $this->assertContains('1', $bindings);
     }
 
 
@@ -215,11 +214,11 @@ class FilterTest extends Orchestra\Testbench\TestCase
     {
         request()->replace([
             'not_in' => ['name' => ['wallacemaxters', 'brcontainer']],
-		]);
+        ]);
 
-		$query = User::query();
+        $query = User::query();
 
-		(new Filter())->apply($query, request());
+        (new Filter())->apply($query, request());
 
         $expected_sql = 'select * from "users" where ("name" not in (?, ?))';
 
@@ -227,8 +226,8 @@ class FilterTest extends Orchestra\Testbench\TestCase
         
         $bindings = $query->getBindings();
 
-		$this->assertContains('wallacemaxters', $bindings);
-		$this->assertContains('brcontainer', $bindings);
+        $this->assertContains('wallacemaxters', $bindings);
+        $this->assertContains('brcontainer', $bindings);
     }
 
 
@@ -237,96 +236,96 @@ class FilterTest extends Orchestra\Testbench\TestCase
         request()->replace([
             'in'     => ['role' => ['1', '2']],
             'not_in' => ['name' => ['wallacemaxters', 'brcontainer']],
-		]);
+        ]);
 
-		$query = User::query();
+        $query = User::query();
 
-		(new Filter())->apply($query, request());
+        (new Filter())->apply($query, request());
 
         $expected_sql = 'select * from "users" where ("role" in (?, ?) and "name" not in (?, ?))';
 
         $this->assertTrue($query->toSql() === $expected_sql);
-	}
-	
-	public function testSetRule()
-	{
-		request()->replace([
-			'my_date_max' => ['updated_at' => '2022-12-31']
-		]);
+    }
+    
+    public function testSetRule()
+    {
+        request()->replace([
+            'my_date_max' => ['updated_at' => '2022-12-31']
+        ]);
 
-		$query = User::query();
+        $query = User::query();
 
-		Filter::make()->setRule('my_date_max', function ($query, $field, $value) {
-			$query->whereDate($field, '<=', $value);
-		})
-		->apply($query, request());
+        Filter::make()->setRule('my_date_max', function ($query, $field, $value) {
+            $query->whereDate($field, '<=', $value);
+        })
+        ->apply($query, request());
 
-		$expected_sql = 'select * from "users" where (strftime(\'%Y-%m-%d\', "updated_at") <= ?)';
-	
-		$this->assertTrue($query->toSql() === $expected_sql);
+        $expected_sql = 'select * from "users" where (strftime(\'%Y-%m-%d\', "updated_at") <= ?)';
+    
+        $this->assertTrue($query->toSql() === $expected_sql);
 
-		$this->assertContains('2022-12-31', $query->getBindings());
-	
-	}
+        $this->assertContains('2022-12-31', $query->getBindings());
+    
+    }
 
-	public function testSetRuleCatchException()
-	{
-		$filter = new Filter();
-		try  {
+    public function testSetRuleCatchException()
+    {
+        $filter = new Filter();
+        try  {
 
-			$filter->setRule('rule', 'invalid_value');
+            $filter->setRule('rule', 'invalid_value');
 
-		} catch (\Exception $e) {
+        } catch (\Exception $e) {
 
-			$this->assertTrue($e instanceof \UnexpectedValueException);
-		}
-	}
-
-
-	public function testDateMax()
-	{
-		request()->replace([
-			'date_max' => ['posted_at' => '2025-12-30']
-		]);
-
-		Filter::make()->apply($query = User::query(), request());
-
-		$expected_sql = 'select * from "users" where (strftime(\'%Y-%m-%d\', "posted_at") <= ?)';
-	
-		$this->assertEquals($query->toSql(), $expected_sql);
-
-		$this->assertContains('2025-12-30', $query->getBindings());
-	
-	}
-
-	public function testDateMin()
-	{
-		request()->replace([
-			'date_min' => ['posted_at' => '1998-10-10']
-		]);
-
-		Filter::make()->apply($query = User::query(), request());
-
-		$expected_sql = 'select * from "users" where (strftime(\'%Y-%m-%d\', "posted_at") >= ?)';
-	
-		$this->assertEquals($query->toSql(), $expected_sql);
-
-		$this->assertContains('1998-10-10', $query->getBindings());
-	
-	}
+            $this->assertTrue($e instanceof \UnexpectedValueException);
+        }
+    }
 
 
-	public function testTraitHasFilter()
-	{
+    public function testDateMax()
+    {
+        request()->replace([
+            'date_max' => ['posted_at' => '2025-12-30']
+        ]);
 
-		// api/users?contains[name]=Wallace
-		request()->replace([
-			'contains' => ['name' => 'Wallace'],
-		]);
+        Filter::make()->apply($query = User::query(), request());
 
-		$query = User::filter();
+        $expected_sql = 'select * from "users" where (strftime(\'%Y-%m-%d\', "posted_at") <= ?)';
+    
+        $this->assertEquals($query->toSql(), $expected_sql);
 
-		$this->assertEquals($query->toSql(), 'select * from "users" where ("name" LIKE ?)');
+        $this->assertContains('2025-12-30', $query->getBindings());
+    
+    }
 
-	}
+    public function testDateMin()
+    {
+        request()->replace([
+            'date_min' => ['posted_at' => '1998-10-10']
+        ]);
+
+        Filter::make()->apply($query = User::query(), request());
+
+        $expected_sql = 'select * from "users" where (strftime(\'%Y-%m-%d\', "posted_at") >= ?)';
+    
+        $this->assertEquals($query->toSql(), $expected_sql);
+
+        $this->assertContains('1998-10-10', $query->getBindings());
+    
+    }
+
+
+    public function testTraitHasFilter()
+    {
+
+        // api/users?contains[name]=Wallace
+        request()->replace([
+            'contains' => ['name' => 'Wallace'],
+        ]);
+
+        $query = User::filter();
+
+        $this->assertEquals($query->toSql(), 'select * from "users" where ("name" LIKE ?)');
+
+    }
 }
