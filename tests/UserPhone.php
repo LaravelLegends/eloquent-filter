2e6f0ac4083b4840e3c08e4a73552505afa1feb6
Wallace Maxters <wallace.vizerra@donadvice.com>
2021-02-03 13:59:34 -0300

    Adding user relation in UserPhone

--- tests/UserPhone.php
+++ tests/UserPhone.php
@@ -5,4 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class UserPhone extends Model
 {
     protected $table = 'user_phones';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
