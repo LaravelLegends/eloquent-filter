a2d7546939fbf8343df6f3de989d0530cf974011
wallacemaxters <wallacemaxters@gmail.com>
2021-11-27 00:28:23 -0300

    Improve facade tests

--- tests/FilterServiceProviderTest.php
+++ tests/FilterServiceProviderTest.php
@@ -1,22 +1,38 @@
 <?php
 
 use LaravelLegends\EloquentFilter\Providers\FilterServiceProvider;
-use Models\User;
+use LaravelLegends\EloquentFilter\Facades\Filter as FilterFacade;
+use LaravelLegends\EloquentFilter\Filter;
+use LaravelLegends\EloquentFilter\Rules\Max;
+use LaravelLegends\EloquentFilter\Rules\Min;
+use Models\Role;
 
 class FilterServiceProviderTest extends Orchestra\Testbench\TestCase
 {
-    public function testProvider()
+    public function testInstanceOf()
     {
-        $filter = app(\LaravelLegends\EloquentFilter\Filter::class);
+        $filter = app(Filter::class);
 
-        $this->assertEquals(get_class($filter), \LaravelLegends\EloquentFilter\Filter::class);
+        $this->assertEquals(get_class($filter), Filter::class);
     }
 
     public function testFacade()
     {
-        $min = \LaravelLegends\EloquentFilter\Facades\Filter::getRule('min');
+        $this->assertEquals(FilterFacade::getRule('min'), Min::class);
+        $this->assertEquals(FilterFacade::getRule('max'), Max::class);
+    }
 
-        $this->assertEquals($min, \LaravelLegends\EloquentFilter\Rules\Min::class);
+    public function testApply()
+    {
+        FilterFacade::apply($query = Role::query(), [
+            'contains' => ['name' => 'Admin']
+        ]);
+
+        $expected = Role::where(function ($query) {
+            $query->where('name', 'LIKE', '%Admin%');
+        })->toSql();
+
+        $this->assertEquals($expected, $query->toSql());
     }
 
     protected function getPackageProviders($app)
