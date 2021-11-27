6fbad880106012f69323655161c91ae5b5df7cb0
wallacemaxters <wallacemaxters@gmail.com>
2021-11-27 00:22:05 -0300

    fix typo: filterables insteadof filterable

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -10,7 +10,7 @@ class User extends Model
 
     protected $table = 'users';
 
-    public $filterable = [
+    protected $filterables = [
         'name'           => 'contains',
         'email'          => true,
         'age'            => ['max', 'min'],
