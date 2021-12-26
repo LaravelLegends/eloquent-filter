da1ee33faa5ccbebebe15f2d85e4d75e81db5469
wallacemaxters <wallacemaxters@gmail.com>
2021-12-26 16:13:14 -0300

    Improve code to generate filter

--- src/eloquent-filter/Console/FilterMakeCommand.php
+++ src/eloquent-filter/Console/FilterMakeCommand.php
@@ -3,6 +3,7 @@
 namespace LaravelLegends\EloquentFilter\Console;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Database\Eloquent\Model;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -15,11 +16,10 @@ class FilterMakeCommand extends GeneratorCommand
 {
     const FIELDS_BY_TYPE = [
         'datetime' => ['date_max', 'date_min', 'year_max', 'year_min', 'year_exact'],
-        'string'   => ['contains', 'starts_with', 'ends_with'],
         'int'      => ['exact', 'min', 'max'],
-        'key'      => ['exact', 'not_exact']
-
-    ];  
+        'key'      => ['exact', 'not_exact'],
+        'string'   => ['contains', 'starts_with', 'ends_with'],
+    ];
 
 
     /**
@@ -72,7 +72,7 @@ class FilterMakeCommand extends GeneratorCommand
     protected function getArguments()
     {
         return [
-            ['name', InputArgument::REQUIRED, 'The name of the command'],
+            ['name', InputArgument::REQUIRED, 'The name of the filter'],
         ];
     }
 
@@ -84,53 +84,70 @@ class FilterMakeCommand extends GeneratorCommand
      */
     protected function buildClass($name)
     {
-        $template = strtr(parent::buildClass($name), ['DummyFilterables' => $this->buildFilterableArray()]);
+        $template = strtr(parent::buildClass($name), [
+            'DummyFilterables' => $this->buildFilterableArray()
+        ]);
 
         return $template;
     }
 
-
-    public function buildFilterableArray(): string
+    protected function buildFilterableFromModel(Model $model): array
     {
+        $casts   = $model->getCasts();
+        $hiddens = $model->getHidden();
 
-        $replacement = $this->buildRulesOfField('id', static::FIELDS_BY_TYPE['key']);
+        $replacements = [];
 
-        if ($modelClass = $this->option('model')) {
-            $model = new $modelClass;
-
-            
-            foreach ($model->getFillable() as $field) {
-
-                if (in_array($field, $model->getHidden())) {
-                    continue;
-                }
-
-                $type = $model->getCasts()[$field] ?? 'string';
-
-                if ($type === 'int' || $type === 'datetime') {
-                    $rules = static::FIELDS_BY_TYPE[$type];
-                } else {
-                    $rules = static::FIELDS_BY_TYPE['string'];
-                }
-
-                $replacement .= $this->buildRulesOfField($field, $rules);
+        $replacements[] = $this->buildRulesOfField(
+            $model->getKeyName(),
+            static::FIELDS_BY_TYPE['key']
+        );
+        
+        foreach ($model->getFillable() as $field) {
+            if (in_array($field, $hiddens)) {
+                continue;
             }
 
-            if ($model->timestamps) {
-                foreach (array_filter([$model->getCreatedAtColumn(), $model->getUpdatedAtColumn()]) as $field) {
-                    $replacement .= $this->buildRulesOfField($field, static::FIELDS_BY_TYPE['datetime']);
-                }
+            $type = $casts[$field] ?? 'string';
+
+            if (in_array($type, ['int', 'datetime'])) {
+                $rules = static::FIELDS_BY_TYPE[$type];
+            } else {
+                $rules = static::FIELDS_BY_TYPE['string'];
+            }
+
+            $replacements[] = $this->buildRulesOfField($field, $rules);
+        }
+
+        if ($model->timestamps) {
+            $timestampsFields = array_filter([
+                $model->getCreatedAtColumn(),
+                $model->getUpdatedAtColumn()
+            ]);
+
+            foreach ($timestampsFields as $field) {
+                $replacements[] = $this->buildRulesOfField(
+                    $field,
+                    static::FIELDS_BY_TYPE['datetime']
+                );
             }
         }
 
-        return '[' . $replacement . str_repeat(' ', 8) . ']';
+        return $replacements;
     }
 
-    public function getOptions()
+
+    protected function buildFilterableArray(): string
     {
-        return [
-            ['model', 'm', InputOption::VALUE_NONE, 'Create the filter basead on a model'],
-        ];
+        if ($modelClass = $this->option('model')) {
+            $replacements = $this->buildFilterableFromModel(new $modelClass);
+        } else {
+            $replacements = [
+                $this->buildRulesOfField('id', static::FIELDS_BY_TYPE['key'])
+            ];
+        }
+
+        return "[\n" . implode("\n", $replacements) . str_repeat(' ', 8) . ']';
     }
 
     protected function buildRulesOfField(string $field, array $rules): string
@@ -139,6 +156,13 @@ class FilterMakeCommand extends GeneratorCommand
         
         $spaces = str_repeat(' ', 12);
 
-        return "\n{$spaces}'{$field}' => [{$rulesAsCode}],\n";
+        return "{$spaces}'{$field}' => [{$rulesAsCode}],\n";
+    }
+
+    public function getOptions()
+    {
+        return [
+            ['model', 'm', InputOption::VALUE_NONE, 'Create the filter basead on a model'],
+        ];
     }
 }
